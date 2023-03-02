0648688e06a0953d02186df7e77194b4d849ab24
emmanuelggggg <Many1997XD@oulook.com>
2023-03-01 20:02:26 -0700

    subiendo mi portafolio

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Menu from './components/templates/Menu.template.vue'
+import Main from './components/templates/Main.template.vue'
+import Footer from './components/templates/Footer.template.vue'
+</script>
+
+<template>
+  <body id="page-top">
+    <!-- navBar -->
+    <header>
+      <Menu />
+    </header>
+  
+    <main>
+      <!-- <Main /> -->
+      <Main />
+    </main>
+
+    <footer class="bg-dark ">
+        <Footer />
+    </footer>
+  </body>
+</template>
+
+
---
+++ src/components/templates/Menu.template.vue
@@ -0,0 +1,30 @@
+
+<template>
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
+        <div class="container px-5">
+            <a class="navbar-brand fw-bold" href="#page-top">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-person-video" viewBox="0 0 16 16">
+                <path d="M8 9.05a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
+                <path d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2Zm10.798 11c-.453-1.27-1.76-3-4.798-3-3.037 0-4.345 1.73-4.798 3H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-1.202Z"/>
+                </svg>
+                </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="bi-list"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto me-4 my-3 my-lg-0">
+                    <li class="nav-item"><a class="nav-link me-lg-3" href="#features">Features</a></li>
+                    <li class="nav-item"><a class="nav-link me-lg-3" href="#download">Download C.V.</a></li>
+                </ul>
+                <button class="btn btn-primary rounded-pill px-3 mb-2 mb-lg-0" data-bs-toggle="modal" data-bs-target="#feedbackModal">
+                    <span class="d-flex align-items-center">
+                        <i class="bi-chat-text-fill me-2"></i>
+                        <span class="small">Send Feedback</span>
+                    </span>
+                </button>
+            </div>
+        </div>
+    </nav>
+</template>
